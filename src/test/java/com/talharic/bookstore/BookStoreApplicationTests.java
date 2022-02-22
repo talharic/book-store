feae46bbe1f669a8f260415265268717aa401e91
Talha Arıç <talharic@gmail.com>
2022-02-22 04:36:29 +0300

    Initial commit

---
+++ src/test/java/com/talharic/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talharic.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
